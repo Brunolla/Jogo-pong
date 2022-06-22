6b8ee0ef04daeb2d53167b3e84749618a8f7c3d4
Brunolla <108034575+Brunolla@users.noreply.github.com>
2022-06-22 20:44:31 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+let xbolinha = 300;
+let ybolinha = 200;
+let diametro = 15;
+
+let velocidadeXbolinha = 6;
+
+function setup() {
+createCanvas(600, 400);
+}
+
+function draw() {
+    background(0);
+    circle(xbolinha, ybolinha, diametro);
+ xbolinha += velocidadexbolinha;
+}
+
+
